81daef381a55114c52ae3af8a6cad006213dc625
Krystle <krystle.leslie.co@gmail.com>
2026-08-17 14:10:40 +0200

    edit prod base_url

--- apps/client/src/api.ts
+++ apps/client/src/api.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080/";
+// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/";
+const BASE_URL = "https://server-production-914b.up.railway.app/";
 
 const api = {
   async getAllActivities() {
